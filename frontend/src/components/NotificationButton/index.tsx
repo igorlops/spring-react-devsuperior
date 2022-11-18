3b47749d9fc055d36ee919d246229c8e9e4a64fe
Igor Lopes <claudioigorai@gmail.com>
2022-11-18 19:19:50 -0300

    Fix React

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/Vector.svg';
 import { BASE_URL } from '../utils/request';
 import './style.css';
+
 
 
 type Props = {
@@ -11,7 +13,7 @@ type Props = {
 function handleClick(id:number){
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("Sucesso!");
+        toast.info("SMS Enviado com sucesso");
     })
 }
 function NotificationButton({saleId}: Props) {
